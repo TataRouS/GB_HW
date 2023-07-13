4e7a86dfcd6c617424106350896f2ce2e88a7c47
Nata Kuznetsova <natanavky@icloud.com>
2023-07-13 23:21:04 +0300

    refactored OneLineTextFieldView and depenenses

--- Store/GBShop/App/Flows/TestViewController.swift
+++ Store/GBShop/App/Flows/TestViewController.swift
@@ -13,7 +13,20 @@ class TestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        
+       
+        let textfield = UITextField()
+        textfield.backgroundColor = .white
+
+        view.addSubview(textfield)
+
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            textfield.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textfield.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            textfield.heightAnchor.constraint(equalToConstant: 30),
+            textfield.widthAnchor.constraint(equalToConstant: 300)
+        ])
         
         //        var textfield = UITextField()
         //
